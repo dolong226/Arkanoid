ee39629d9744b771940a037d4cfa4fc5902d1b36
John Davis <vudinhtuan3032006@gmail.com>
2025-10-25 16:07:26 +0700

    Cập nhật các định nghĩa class liên quan tới bóng, xử lí va chạm, và thanh paddle

--- src/main/java/ball/Velocity.java
+++ src/main/java/ball/Velocity.java
@@ -1,7 +1,130 @@
 package ball;
-
+import geometry.Point;
 /**
- * rỗng.
+ * Class này định nghĩa về vận tốc của bóng khi di chuyển, tính toán vị trí tiếp theo của bóng khi di chuyển
  */
 public class Velocity {
+    /**
+     * Vận tốc tính theo chiều trục x
+     */
+    private double Dx;
+    /**
+     * Vận tốc tính theo chiều trục y
+     */
+    private double Dy;
+
+    /**
+     * Khởi tạo vận tốc
+     * @param dx vận tốc theo chiều x
+     * @param dy vận tốc theo chiều y
+     */
+    public Velocity(double dx, double dy){
+        this.Dx = dx;
+        this.Dy = dy;
+    }
+
+    /**
+     * Hàm này tính toán và trả về vận tốc theo chiều x, y tương ứng với hướng bóng đang di chuyển, dựa trên góc của đường bóng đi tạo với trục y hướng lên  
+     * @param angle Góc của đường bóng đi so với trục y hướng lên
+     * @param speed Tốc độ của bóng
+     * @return
+     */
+    public static Velocity fromAngleAndSpeed(double angle, double speed){
+        /**
+         * Nếu góc nhỏ hơn không (âm)
+         */
+        while(angle < 0){
+            angle += 360;
+        }
+        /**
+         * Nếu góc quá 360 độ
+         */
+        while(angle > 360){
+            angle -= 360;
+        }
+
+        /**
+         * Nếu góc từ khoảng 0 đến 90 độ --> bóng đang đi theo chiều giảm của y và chiều tăng x (lên trên sang phải)
+         */
+        if(angle >=0  && angle <= 90){
+            angle = Math.toRadians(angle);
+            double dx = speed * Math.sin(angle);
+            double dy = -speed * Math.cos(angle);
+            return new Velocity(dx, dy);
+        }
+
+        /**
+         * Nếu góc từ 90 đến 180 độ --> bóng đang đi theo chiều tăng của y và chiều tăng của x (xuống dưới sang phải)
+         */
+        else if(angle > 90 && angle <= 180){
+            angle = angle - 90;
+            angle = Math.toRadians(angle);
+            double dx = speed * Math.cos(angle);
+            double dy = speed * Math.sin(angle);
+            return new Velocity(dx, dy);
+        }
+
+        /**
+         * Nếu góc từ 180 đến 270 độ --> bóng đang đi theo chiều tăng của y và chiều giảm của x (xuống dưới sang trái)
+         */
+        else if(angle > 180 && angle <= 270){
+             angle = angle - 180;
+             angle = Math.toRadians(angle);
+             double dx = -speed * Math.sin(angle);
+             double dy = speed * Math.cos(angle);
+             return new Velocity(dx, dy); 
+        }
+
+        /**
+         * Nếu góc từ 270 đến 360 độ --> bóng đang đi theo chiều giảm của y và chiều giảm của x (lên trên sang trái)
+         */
+        else{
+            angle = angle - 270;
+            angle = Math.toRadians(angle);
+            double dx = -speed * Math.cos(angle);
+            double dy = -speed * Math.sin(angle);
+            return new Velocity(dx, dy);
+        }
+    }
+
+    /**
+     * Hàm này đưa ra vị trí bóng tiếp theo sau khi cập nhật vận tốc
+     * @param onePoint điểm hiện tại
+     * @return điểm mới ở vị trí mới
+     */
+    public Point applyToPoint(Point onePoint){
+        return new Point(onePoint.getX() + Dx, onePoint.getY() + Dy);
+    }
+
+    /**
+     * Trả về giá trị vận tốc theo x
+     * @return vận tốc theo x
+     */
+    public double getDx(){
+        return this.Dx;
+    }
+
+    /**
+     * Trả về giá trị vận tốc theo y
+     * @return vận tốc theo y
+     */
+    public double getDy(){
+        return this.Dy;
+    }
+
+    /**
+     * Set giá trị vận tốc theo x
+     * @param dx vận tốc theo x
+     */
+    public void setDx(double dx){
+        this.Dx = dx;
+    }
+
+    /**
+     * Set giá trị vận tốc theo y
+     * @param dy vận tốc theo y
+     */
+    public void setDy(double dy){
+        this.Dy = dy;
+    }
 }
--- src/main/java/collidable/Block.java
+++ src/main/java/collidable/Block.java
@@ -1,16 +1,186 @@
 package collidable;
 
+import java.util.List;
+
+
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
+
 import game.Sprite;
 import javafx.scene.canvas.GraphicsContext;
+import listener.HitNotifier;
+import ball.*;
+import geometry.*;
+import listener.HitEvent;
+import listener.HitListener;
 
-public class Block implements Sprite {
+/**
+ * Hàm này định nghĩa về block (khối hình) trong trò chơi, cập nhật khối hình sau sự kiện, tính toán vận tốc của bóng sau khi va chạm và thông báo sự kiện va chạm
+ */
+
+public class Block implements Sprite, Collidable, HitNotifier {
+    /**
+     * Danh sách thông báo sự kiện va chạm
+     */
+    private List<HitListener> hitListeners;
+    /**
+     * Khối block
+     */
+    private Rectangle rectangle;
+    /**
+     * Màu khối
+     */
+    private Color color;
+    
+    /**
+     * Khởi tạo block
+     * @param rectangle Khối block
+     * @param color Màu
+     */
+    public Block(Rectangle rectangle, Color color) {
+        this.color = color;
+        this.rectangle = rectangle;
+        hitListeners = new ArrayList<>();
+    }
+
+    /**
+     * Khởi tạo khối Block
+     * @param leftUpperCorner Điểm góc trên trái của khối block
+     * @param width Chiều dài
+     * @param height Chiều rộng
+     */
+    public Block(Point leftUpperCorner, int width, int height) {
+        this.rectangle = new Rectangle(leftUpperCorner, width, height);
+        hitListeners = new ArrayList<>();
+    }
+    
+    /**
+     * Vẽ lại khối block
+     */
     @Override
     public void render(GraphicsContext gc) {
+        gc.setFill(color);
+        double upperLeftX = rectangle.getUpperLeft().getX();
+        double upperLeftY = rectangle.getUpperLeft().getY();
+        double length = rectangle.getLength();
+        double width = rectangle.getWidth();
+        gc.fillRect(upperLeftX, upperLeftY, length, width);
+    }
 
+    /**
+     * Update lại hình ảnh block sau thời gian dt
+     */
+    @Override
+    public void update(double dt) {
+    }
+
+    /**
+     * Thay đổi vận tốc bóng sau khi va chạm với block dựa trên hướng bóng va chạm
+     * @param hitter Bóng va chạm
+     * @param collisionPoint Điểm va chạm
+     * @param currentVelocity Vận tốc bóng hiện tại
+     * @return updateVelocity Vận tốc mới 
+     */
+    public Velocity hit(Ball hitter, Point collisionPoint, Velocity currentVelocity){
+        Velocity upDateVelocity = new Velocity(currentVelocity.getDx(), currentVelocity.getDy());
+
+        double upperLeftX = this.rectangle.getUpperLeft().getX();
+        double upperLeftY = this.rectangle.getUpperLeft().getY();
+
+        double x = collisionPoint.getX();
+        double y = collisionPoint.getY();
+        double epsilon = 0.0001;
+        boolean dxChanged = false;
+        boolean dyChanged = false;
+
+        /**
+         * Nếu bóng va vào cạnh trên và dưới của block
+         */
+        if(Math.abs(y - upperLeftY) < epsilon || Math.abs(y - (upperLeftY + this.rectangle.getWidth())) < epsilon){
+            /**
+             * Bóng va vào giữa cạnh (không tính hai đỉnh biên)
+             */
+            if(x > upperLeftX && x < upperLeftX + this.rectangle.getLength()){
+            upDateVelocity.setDy(currentVelocity.getDy() * (-1));
+            dyChanged = true;
+        }
+    }
+
+        /**
+         * Nếu bóng va vào cạnh bên trái và phải của block
+         */
+        if(Math.abs(x - upperLeftX) < epsilon || Math.abs(x - (upperLeftX + this.rectangle.getLength())) < epsilon){
+            /**
+             * Bóng va vào giữa cạnh (Không tính hai đỉnh biên)
+             */
+            if(y > upperLeftY && y < upperLeftY + this.rectangle.getWidth()){
+            upDateVelocity.setDx(currentVelocity.getDx() * (-1));
+            dxChanged = true;
+        }
+    }
+        /**
+         * Nếu bóng va chạm vào góc 
+         */
+        if(!dyChanged && !dxChanged){
+            upDateVelocity.setDx(currentVelocity.getDx() * (-1));
+            upDateVelocity.setDy(currentVelocity.getDy() * (-1));
+        }
+        /**
+         * Nếu bóng va chạm rất gần góc (để loại trừ sai số của epsilon)
+         */
+        else if((Math.abs(y - upperLeftY) < epsilon || Math.abs(y - (upperLeftY + this.rectangle.getWidth())) < epsilon) && (Math.abs(x - upperLeftX) < epsilon || Math.abs(x - (upperLeftX + this.rectangle.getLength())) < epsilon)){
+            if(!dxChanged){
+                upDateVelocity.setDx(currentVelocity.getDx() * (-1));
+            }
+            if(!dyChanged){
+                upDateVelocity.setDy(currentVelocity.getDy() * (-1));
+        }
+    }
+        this.notifyHit(hitter, collisionPoint);
+        return upDateVelocity;
     }
 
     @Override
-    public void update(double dt) {
+    /**
+     * Thêm block là 1 vật thông báo va chạm 
+     */
+    public void addHitListener(HitListener h){
+        this.hitListeners.add(h);
+    }
 
+    @Override
+    /**
+     * Xóa block khỏi danh sách thông báo va chạm
+     */
+    public void removeHitListener(HitListener h){
+        this.hitListeners.remove(h);
+    }
+
+    /**
+     * Hàm được gọi mỗi khi có va chạm Hit() và thông báo cho toàn bộ các đối tượng quan tâm đến va chạm
+     * @param hitter Bóng
+     * @param collisionPoint Điểm va chạm
+     */
+    public void notifyHit(Ball hitter, Point collisionPoint){
+        List<HitListener> copyHitListeners = new ArrayList<HitListener>(this.hitListeners);
+        for(HitListener hl : copyHitListeners){
+            hl.hitEvent(new HitEvent(hitter, this, collisionPoint));
+        }
+    }
+
+    /**
+     * hàm trả về block
+     * @return rectangle Block
+     */
+    public Rectangle getCollisionRectangle(){
+        return this.rectangle;
+    }
+
+    /**
+     * Hàm trả về màu 
+     * @return color Màu
+     */
+    public Color getColor(){
+        return this.color;
     }
 }
